521a3db4f0f3a14faac228f9764a517871d7ba48
Balati Balati <bbalati01@gmail.com>
2025-03-08 00:49:11 +0400

    Add initial project files including configuration, styles, and components

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Server is running.");
 });
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended');
+    });
+
+    socket.on("calluser", ( {userToCall, signalData, from, name} ) => {
+        io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+    });
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
