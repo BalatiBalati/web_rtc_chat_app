3814f61aa5e580d0b550388dbe6f649673211c7b
Balati Balati <bbalati01@gmail.com>
2025-03-08 14:31:53 +0400

    Refactor App component structure; add VideoPlayer, Notifications, and Options components; update ReactDOM rendering method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
+import { Typography, AppBar, makeStyles } from '@mui/material';
+
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+
+const useStyles = makeStyles((theme) => ({ 
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 100px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '600px',
+        border: '2px solid black',
+    
+        [theme.breakpoints.down('xs')]: {
+          width: '90%',
+        },
+    },
+    
+    image: {
+        marginLeft: '15px',
+    },
+    
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+    },
+}));
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center" color="primary">Video Chat</Typography>
+        <div className={classes.wrapper}>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography variant="h2" align="center" color="primary">
+                    Video Chat
+                </Typography>
             </AppBar>
+            <VideoPlayer />
+            <Options>
+                <Notifications />
+            </Options>
         </div>
     );
-}
+};
 
 export default App;
